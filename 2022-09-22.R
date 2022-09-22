153edb71705eff79b736dd1d83c42794b94cc03e
Woodruff <bmwoodruff@gmail.com>
2022-09-22 11:20:01 -0600

    after class sep 22

---
+++ 2022-09-22.R
@@ -0,0 +1,20 @@
+#Brain Gain 3
+
+i <- seq(3,7)
+2+i^2
+sum(2+i^2)
+1/3*sum(2+i^2)
+
+#Brain Gains 1 and 2
+k <- seq(4,8)
+k <- 4:8
+sum(5-k)
+
+
+prod(1:4)
+n <- seq(1,4)
+prod(n)
+
+factorial(4)
+
+
